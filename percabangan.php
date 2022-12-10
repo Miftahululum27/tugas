9edbaf5c5d99e882a4b3cfa56ccb0564de741b7e
MIFTAHUL ULUM <114923699+Miftahululum27@users.noreply.github.com>
2022-12-10 21:17:19 +0700

    Add files via upload

---
+++ percabangan.php
@@ -0,0 +1,21 @@
+<?php 
+
+$cv = 3;
+
+switch($cv){
+    case 1:
+        echo "081878276588";
+        break;
+    case 2:
+        echo "Aris Setiawan";
+        break;
+    case 3:
+        echo "rizky@gmail.com";
+        break;
+    case 4:
+        echo "cisaat";
+        break;
+    default:
+        echo "Bukan Salah satu diatas";
+}
+?>
